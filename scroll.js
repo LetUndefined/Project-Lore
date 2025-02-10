235ed9de6271f8231acbe04a6efeb2603138b6ae
Sandro <sandroodangelo@hotmail.com>
2025-02-10 19:10:39 +0100

    fixed mail adres

--- scroll.js
+++ scroll.js
@@ -13,15 +13,5 @@ function sendMail() {
   let subjectLine = document.getElementById("subject").value;
   let name = document.getElementById("name").value;
   let phone = document.getElementById("phone").value;
-  window.location.href =
-    "mailto:lore.opsteyn@gmail.com?subject=" +
-    subjectLine +
-    "&body=" +
-    "Naam: " +
-    name +
-    "%0D%0A" +
-    "Telefoonnummer: " +
-    phone +
-    "%0D%0A" +
-    body;
+  window.location.href = "mailto:atelierlore25@gmail.com?subject=" + subjectLine + "&body=" + "Naam: " + name + "%0D%0A" + "Telefoonnummer: " + phone + "%0D%0A" + body;
 }
